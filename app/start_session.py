234d07c8e9a6b0f89b15e93dd7173b18f42b5628
Thibault Derousseaux <6574550+tibdex@users.noreply.github.com>
2026-05-28 17:01:46 -0400

    Switch to in-memory user content storage

--- app/start_session.py
+++ app/start_session.py
@@ -2,34 +2,15 @@ import sys
 from asyncio import to_thread
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
-from pathlib import Path
 
 import atoti as tt
 import httpx
-from atoti_jdbc import UserContentStorageConfig
 
 from .config import Config
 from .create_and_join_tables import create_and_join_tables
 from .create_cubes import create_cubes
 from .load_tables import load_tables
 from .opentelemetry import span
-
-
-def _get_session_config(config: Config, /) -> tt.SessionConfig:
-    user_content_storage: Path | UserContentStorageConfig | None = None
-
-    if config.user_content_storage is not None:
-        user_content_storage = (
-            config.user_content_storage
-            if isinstance(config.user_content_storage, Path)
-            else UserContentStorageConfig(url=str(config.user_content_storage))
-        )
-
-    return tt.SessionConfig(
-        logging=tt.LoggingConfig(destination=sys.stdout),
-        port=config.port,
-        user_content_storage=user_content_storage,
-    )
 
 
 @span
@@ -45,7 +26,10 @@ async def start_session(
     http_client: httpx.AsyncClient,
 ) -> AsyncGenerator[tt.Session]:
     """Start the session, declare the data model and load the initial data."""
-    session_config = _get_session_config(config)
+    session_config = tt.SessionConfig(
+        logging=tt.LoggingConfig(destination=sys.stdout),
+        port=config.port,
+    )
     session = await to_thread(tt.Session.start, session_config)
     try:
         with tt.mapping_lookup(check=config.check_mapping_lookups):
