dff1a7500f360c86a7d2b53290fd9810e3a2c670
Thibault Derousseaux <6574550+tibdex@users.noreply.github.com>
2026-07-27 16:35:42 -0400

    Simplify

--- tests/docker/test_docker.py
+++ tests/docker/test_docker.py
@@ -1,5 +1,4 @@
 import atoti as tt
-import pandas as pd
 
 from app import Skeleton
 
@@ -13,9 +12,6 @@ def test_session_inside_docker_container(
     cube = session_inside_docker_container.cubes[skeleton.name]
     m = cube.measures
     result = cube.query(m[skeleton.measures.CAPACITY.name])
-    assert isinstance(  # Remove once ty detects it on its own.
-        result, pd.DataFrame
-    )
     total_capacity = result[skeleton.measures.CAPACITY.name][0]
     assert total_capacity > EXPECTED_TOTAL_CAPACITY, (
         "The data fetched from the external API should lead to a greater capacity than the one of the local data since new stations have been created since the data was snapshotted."
